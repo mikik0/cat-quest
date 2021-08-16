78016e536790d5424106efdc8ab2e08e10a456f3
佐伯周亮 <saekisyusuke@nebokeneko.local>
2021-08-17 02:03:47 +0900

    routes修正 & authenticate_user追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  # root 'devise/sessions#new'
-  devise_for :users
+  devise_for :users, skip: :all
+  devise_scope :user do
+    root 'devise/sessions#new'
+    get  'login' => 'devise/sessions#new'
+    post 'login' => 'devise/sessions#create'
+    delete 'logout' => 'devise/sessions#destroy'
+    get  'signup' => 'devise/registrations#new'
+    post  'signup' => 'devise/registrations#create'
+  end
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
