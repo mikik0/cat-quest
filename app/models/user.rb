6d7da2b48241921bc74985a72b67dc0d61ac1aa3
佐伯周亮 <saekisyusuke@nebokeneko.local>
2021-08-16 23:59:44 +0900

    ログイン確認用mypage作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          authentication_keys: [:name]
 
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if name = conditions.delete(:name)
-      where(conditions).where(username: username).first
-    else
-      where(conditions).first
-    end
-  end
-
 # No use email
   def email_required?
     false
