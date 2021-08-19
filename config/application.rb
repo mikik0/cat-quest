712bd067f5bf863b3acf67c6d345e7cb14dc9f89
mona-apk <cordion_nap@yahoo.co.jp>
2021-08-19 22:34:11 +0900

    chore: 日本語化ファイル用のコンフィグ設定

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module Nekoquest
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # エラーメッセージの日本語化
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
