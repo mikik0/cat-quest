7cbd5be67e8b2ba7bac50f052483e8aeb2d6af44
佐伯周亮 <saekisyusuke@nebokeneko.local>
2021-08-20 17:03:55 +0900

    userのshowページ、機能部分のみ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def show
+    @user = current_user
+    @character = @user.character
+    @current_quest,@past_quest = @user.quest_time_sort
   end
+
 
   def edit
   end
