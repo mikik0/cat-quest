e1bf11ddaf6980b811afede9be9768ec4689b1a9
mikik0 <usapon_love1222@yahoo.co.jp>
2021-08-22 14:19:23 +0900

    色々間違ってるかも

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,4 +1,5 @@
 class Quest < ApplicationRecord
 	has_many :contents
 	has_many :user_quests, through: :contents
+	has_many :user_contents
 end
--- app/models/user_content.rb
+++ app/models/user_content.rb
@@ -1,3 +1,5 @@
 class UserContent < ApplicationRecord
   belongs_to :user
+  belongs_to :content
+  belongs_to :quest
 end
