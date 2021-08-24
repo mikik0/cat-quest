066052ed946cdd54398f6c580f9e2559b46c735c
mikik0 <usapon_love1222@yahoo.co.jp>
2021-08-24 17:21:11 +0900

    スキーマ修正

--- db/migrate/20210816173234_create_contents.rb
+++ db/migrate/20210816173234_create_contents.rb
@@ -3,7 +3,7 @@ class CreateContents < ActiveRecord::Migration[5.2]
     create_table :contents do |t|
       t.references :quest, foreign_key: true
       t.string :youtube_url
-      t.string :video_time
+      # t.string :video_time
 
       t.timestamps
     end
