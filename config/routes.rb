9724d18174fcc98174a9daed318f6d989a8045ac
佐伯周亮 <saekisyusuke@nebokeneko.local>
2021-08-24 22:06:05 +0900

    routesとそれに伴う一部変更

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
     post  'signup' => 'devise/registrations#create'
   end
 
-  resources :users, only: [:show, :edit, :update] do
-    resources :quests, only: [:index, :show, :edit, :update, :delete] do
-      resources :content
+  resources :users, only: [:show, :edit, :update]
+
+  resources :myquests, only: [:index], controller: :users do
+    resources :contents, only: [:show] do
+      resources :memos, only: [:create, :destroy]
     end
   end
 
