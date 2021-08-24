3a56a76e48275f3f5cbf77e9da5a953f9e93f1f9
mikik0 <usapon_love1222@yahoo.co.jp>
2021-08-24 19:32:06 +0900

    URLの削除・更新もできるようになりました🎉

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,7 +1,7 @@
 class Quest < ApplicationRecord
   has_many :user_quests, dependent: :destroy
   has_many :contents, dependent: :destroy
-  accepts_nested_attributes_for :contents
+  accepts_nested_attributes_for :contents, allow_destroy: true
 
   def current_quest?
      finished_at > Time.now
