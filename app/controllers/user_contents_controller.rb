a0df3919c1e853dd9d67eab4e021e52212d449c0
mikik0 <usapon_love1222@yahoo.co.jp>
2021-08-25 21:39:18 +0900

    見たかどうか判定できるようになったよ！

--- app/controllers/user_contents_controller.rb
+++ app/controllers/user_contents_controller.rb
@@ -2,18 +2,16 @@ class UserContentsController < ApplicationController
     def create
       @content = Content.find(params[:content_id])
       @quest = Quest.find(params[:myquest_id])
-      @user_contents = UserContent.new(user_content_params)
+      @user_contents = UserContent.new(user_contents_params)
       @user_contents.user_id = current_user.id
       @user_contents.content_id = params[:content_id]
       @user_contents.is_finished = true
       @user_contents.save
-      redirect_to myquest_content_path(@quest, @content)
     end
 
     def destroy
       @user_content = UserContent.find(params[:id])
       @user_content.destroy
-      redirect_to myquest_content_path
     end
 
     private
