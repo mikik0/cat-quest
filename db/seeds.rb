b02e0b8b8e8848b576ec22475e58c878fde7fde2
佐伯周亮 <saekisyusuke@nebokeneko.local>
2021-08-28 10:33:08 +0900

    seedにねこ登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,9 @@
 
 
   [
-    [ 1, '初心者ねこ', "https://res.cloudinary.com/dmt03c6vy/image/upload/c_scale,w_200/v1630068446/nekochan_otugzi.png"],
-    [ 5, '大人ねこ', ""],
+    [ 1, '赤ちゃんねこ', "https://res.cloudinary.com/dmt03c6vy/image/upload/c_scale,w_200/v1630113605/cat01_wmh89w.png"],
+    [ 5, '大人ねこ', "https://res.cloudinary.com/dmt03c6vy/image/upload/c_scale,w_200/v1630113605/cat02_g7116a.png"],
+    [ 20, 'ライオンねこ', "https://res.cloudinary.com/dmt03c6vy/image/upload/c_scale,w_200/v1630113605/cat03_y3foef.png"]
   ].each do |level, name, image_id|
     Character.create!(
       { level: level, name: name, image_id: image_id }
@@ -25,7 +26,19 @@
     [ 6, 36],
     [ 7, 44],
     [ 8, 52],
-    [ 9, 60]
+    [ 9, 60],
+    [ 10, 68],
+    [ 11, 76],
+    [ 12, 84],
+    [ 13, 92],
+    [ 14, 100],
+    [ 15, 108],
+    [ 16, 116],
+    [ 17, 124],
+    [ 18, 132],
+    [ 19, 140],
+    [ 20, 148],
+    [ 21, 9999999]
   ].each do |level, threshold|
     LevelThreshold.create!(
       { level: level, threshold: threshold }
